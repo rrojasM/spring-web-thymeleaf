3551ed45b778c432e76dafe21b49eceb7d3c5d0a
Rufino Rojas <ruf_rm@hotmail.com>
2022-10-25 18:42:42 -0500

    config security

--- src/main/java/com/rrojas/SpringWebEmpleos/security/DatabaseWebSecurity.java
+++ src/main/java/com/rrojas/SpringWebEmpleos/security/DatabaseWebSecurity.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -22,5 +23,18 @@ public class DatabaseWebSecurity extends WebSecurityConfigurerAdapter {
 						+ "inner join Usuarios u on u.id = up.idUsuario "
 						+ "inner join Perfiles p on p.id = up.idPerfil " + "where u.username = ?");
 
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				// Los recursos estáticos no requieren autenticación
+				.antMatchers("/bootstrap/**", "/images/**", "/tinymce/**", "/logos/**").permitAll()
+				// Las vistas públicas no requieren autenticación
+				.antMatchers("/", "/signup", "/search", "/vacantes/view/**").permitAll()
+				// Todas las demás URLs de la Aplicación requieren autenticación
+				.anyRequest().authenticated()
+				// El formulario de Login no requiere autenticacion
+				.and().formLogin().permitAll();
 	}
 }
